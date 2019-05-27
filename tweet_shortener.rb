0a280b55253d87b1f7b5e21a8e42c8f0a0d1f93b
aeisha simmons <ashicks2017@outlook.com>
2019-05-27 23:40:19 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def dictionary
+  substitute = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweets)
+  
+ tweets.split(",") do |word|
+   if dictionary.key.include? (word.downgrade)
+     word = dictionary[word.downgrade]
+  else
+    word
+    end
+  end.join(" ")
+end
+
+word_substituter()
