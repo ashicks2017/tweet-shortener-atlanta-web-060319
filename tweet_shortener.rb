8f7a5342a269c57dd7fefc6c5aee8b85ef69a7ca
aeisha simmons <ashicks2017@outlook.com>
2019-05-27 23:49:56 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -33,7 +33,7 @@ def word_replace(tweet)
       else
        print word
     end
-  end.join(" ")
+  end
 end
 
 word_replace("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
